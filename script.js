61f7ff0398c8b496ba127147ef0e6514ff1b739c
Stabyy <2000stasbabych2000@gmail.com>
2017-10-17 18:41:46 +0300

    homework final

--- script.js
+++ script.js
@@ -1,51 +1,42 @@
 var button = document.querySelectorAll("button")[0];
 var inputs = document.querySelectorAll("input");
-var oneInp = document.querySelector("#text1");
-var twoInp = document.querySelector("#text2");
-var threeInp = document.querySelector("#text3");
-var fourInp = document.querySelector("#text4");
+// var oneInp = document.querySelector("#text1");
+// var twoInp = document.querySelector("#text2");
+// var threeInp = document.querySelector("#text3");
+// var fourInp = document.querySelector("#text4");
 
-function cFind() {
-var empty = 0;
+var nName = document.querySelectorAll('[name="F-name"], [name="L-name"], [name="email"], [name="phone"]');
 
-for(i = 0; i < inputs.length; i++) {
+// function cFind() {
 
-	
-	if(inputs[i].value.trim() =="") {
-		empty = 1;
-}
+
+// for (var i=0; i< nName.length; i++) {
+//   if (nName[i].value.trim() === "") {
+//     alert('Поле ' + nName[i].name + " is empty!")
+//   } else {
+//   	alert("Відправлено")
+
+//    }
+//   }
+// }
+
+
+function save() {
+	for (var i = 0; i < nName.length; i++) {
+		var value = nName[i].value.trim();
+		var fName = nName[i].name;
+		if (value === "") {
+			alert('Поле ' + nName[i].name + " is empty!")	
+		} else {
+			alert("Відправлено "+ nName[i].name )
+			localStorage.setItem(fName, value);
+		}
+
 	}
-
-		if (oneInp.value.trim() == "") {
-			alert("Ви не заповнили поле F-name");
-		}
-		if (twoInp.value.trim() =="") {
-			alert("Ви не заповнили поле L-name");
-		}
-		if (threeInp.value.trim() =="") {
-			alert("Ви не заповнили поле Email Address");
-		}
-		if (fourInp.value.trim() =="") {
-			alert("Ви не заповнили поле Phone");
-		}
-
-
 }
 
-var elements = document.querySelectorAll('input, textarea');
 
-function checkValidity() {};
 
-for (i=0; i<elements.length; i++) {
- (function(element) {
-   var id = element.getAttribute('id');
-   element.value = localStorage.getItem(id); 
-   element.oninput = function() {
-     localStorage.setItem(id, element.value);
-     checkValidity();
-   };
- })(elements[i]);
-}
 
 
 
